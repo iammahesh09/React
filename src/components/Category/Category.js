3c9df0ea64d14fedb97a7015df344abf466b2e32
iammahesh09 <iammahesh.nlr@gmail.com>
2021-03-25 21:43:20 +0530

    HOC - props and state data Pass

--- src/components/Category/Category.js
+++ src/components/Category/Category.js
@@ -5,7 +5,15 @@ class Category extends Component {
     render() {
         return (
             <React.Fragment>
+                <h4>{this.props.title}</h4>
                 <div>This is Category component - {this.props.name}</div>
+                <ul>
+                    {this.props.categorys.map((category, i) => {
+                        return (
+                            <li key={i}>{category}</li>
+                        )
+                    })}
+                </ul>
             </React.Fragment>
         );
     }
--- src/components/Category/WithCategory.js
+++ src/components/Category/WithCategory.js
@@ -2,11 +2,18 @@ import React, { Component } from "react";
 
 export function WithCategory(WrappedComponent) {
     return class extends Component {
+        constructor(props) {
+            super(props);
+            this.state = {
+                title: "Title for HOC",
+                categorys: ['CategoryOne', 'CategoryTwo', 'CategoryThree', 'CategoryFour']
+            }
+        }
         render() {
             return (
                 <React.Fragment>
                     <div>this is WithCategory Component</div>
-                    <WrappedComponent {...this.props} />
+                    <WrappedComponent {...this.props} {...this.state} />
                 </React.Fragment>
             )
         }
